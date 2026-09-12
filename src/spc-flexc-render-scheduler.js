ad25e3b6966947e95580a91b7c53c229355ad0f5
minimicro34 <minimicro34@gmail.com>
2026-09-12 17:01:06 +0200

    Coalesce card renders on animation frames

---
+++ src/spc-flexc-render-scheduler.js
@@ -0,0 +1,43 @@
+/* SPC FlexC Card v1.0.4 render scheduler. */
+
+const spcFlexCImmediateRender = SpcFlexCCard.prototype._render;
+const spcFlexCBaseDisconnectedCallback =
+  SpcFlexCCard.prototype.disconnectedCallback;
+
+SpcFlexCCard.prototype._render = function () {
+  if (this._spcRenderFrame != null) {
+    return;
+  }
+
+  const render = () => {
+    this._spcRenderFrame = null;
+    this._spcRenderUsesAnimationFrame = false;
+    spcFlexCImmediateRender.call(this);
+  };
+
+  if (typeof window.requestAnimationFrame === "function") {
+    this._spcRenderUsesAnimationFrame = true;
+    this._spcRenderFrame = window.requestAnimationFrame(render);
+  } else {
+    this._spcRenderUsesAnimationFrame = false;
+    this._spcRenderFrame = window.setTimeout(render, 0);
+  }
+};
+
+SpcFlexCCard.prototype.disconnectedCallback = function () {
+  if (this._spcRenderFrame != null) {
+    if (
+      this._spcRenderUsesAnimationFrame &&
+      typeof window.cancelAnimationFrame === "function"
+    ) {
+      window.cancelAnimationFrame(this._spcRenderFrame);
+    } else {
+      window.clearTimeout(this._spcRenderFrame);
+    }
+
+    this._spcRenderFrame = null;
+    this._spcRenderUsesAnimationFrame = false;
+  }
+
+  spcFlexCBaseDisconnectedCallback.call(this);
+};
