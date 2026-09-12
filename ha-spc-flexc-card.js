659bf5444bfba5f092ca265bcb920601a63119b4
Nicolas Chantrein <minimicro34@gmail.com>
2026-09-12 17:51:46 +0200

    Build card with visible version

--- ha-spc-flexc-card.js
+++ ha-spc-flexc-card.js
@@ -4225,6 +4225,25 @@ const spcFlexCImmediateRender = SpcFlexCCard.prototype._render;
 const spcFlexCBaseDisconnectedCallback =
   SpcFlexCCard.prototype.disconnectedCallback;
 
+SpcFlexCCard.prototype._showCardVersion = function () {
+  const diagnosticsBlock = this.querySelector(".diagnostics-block");
+
+  if (!diagnosticsBlock || diagnosticsBlock.querySelector(".spc-card-version")) {
+    return;
+  }
+
+  const versionRow = document.createElement("div");
+  versionRow.className = "diagnostic-row spc-card-version";
+  versionRow.innerHTML = `
+    <ha-icon class="muted" icon="mdi:card-account-details-outline"></ha-icon>
+    <div class="diagnostic-main">
+      <div class="diagnostic-name">SPC FlexC Card</div>
+      <div class="diagnostic-detail muted">Version ${CARD_VERSION}</div>
+    </div>
+  `;
+  diagnosticsBlock.appendChild(versionRow);
+};
+
 SpcFlexCCard.prototype._render = function () {
   if (this._spcRenderFrame != null) {
     return;
@@ -4234,6 +4253,7 @@ SpcFlexCCard.prototype._render = function () {
     this._spcRenderFrame = null;
     this._spcRenderUsesAnimationFrame = false;
     spcFlexCImmediateRender.call(this);
+    this._showCardVersion();
   };
 
   if (typeof window.requestAnimationFrame === "function") {
